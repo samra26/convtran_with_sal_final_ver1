f32636dfa1a198114b121d50fdf19fbc9f744cc3
samra26 <75056386+samra26@users.noreply.github.com>
2022-07-15 03:11:14 +0500

    Update solver.py

--- solver.py
+++ solver.py
@@ -8,7 +8,10 @@ import time
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.utils import make_grid
 writer = SummaryWriter('log/run' + time.strftime("%d-%m"))
-
+import torch.nn as nn
+import argparse
+import os.path as osp
+import os
 size_coarse = (20, 20)
 
 
@@ -88,7 +91,7 @@ class Solver(object):
                     depth = depth.to(device)
 
                 input = torch.cat((images, depth), dim=0)
-                preds,sal_lde_conv,sal_lde_tran,sal_gde_conv,sal_gde_tran, pred_coarse = self.net(input)
+                preds, pred_coarse = self.net(input)
                 #print(preds.shape)
                 preds = F.interpolate(preds, tuple(im_size), mode='bilinear', align_corners=True)
                 pred = np.squeeze(torch.sigmoid(preds)).cpu().data.numpy()
@@ -100,7 +103,49 @@ class Solver(object):
         time_e = time.time()
         print('Speed: %f FPS' % (img_num / (time_e - time_s)))
         print('Test Done!')
+    
+    def val(self):
+        #print('validating model..')
+        
+        self.net.eval()
+        avg_mae = 0.0
+        r_sal_loss = 0
+        rv_loss_item=0
+        with torch.no_grad():
+            for i, data_batch in enumerate(self.val_loader):
+                sal_image, sal_depth, sal_label,name,im_size= data_batch['sal_image'], data_batch['sal_depth'], data_batch[
+                    'sal_label'], data_batch['name'][0], np.asarray(data_batch['size'])
+                if (sal_image.size(2) != sal_label.size(2)) or (sal_image.size(3) != sal_label.size(3)):
+                    print('IMAGE ERROR, PASSING```')
+                    continue
+                if self.config.cuda:
+                    device = torch.device(self.config.device_id)
+                    sal_image, sal_depth, sal_label= sal_image.to(device), sal_depth.to(device), sal_label.to(device)
+                sal_label_coarse = torch.cat((sal_label, sal_label), dim=0)
+                
+                sal_input = torch.cat((sal_image, sal_depth), dim=0)
+                sal_final, sal_coarse = self.net(sal_input)
+                
+                sal_loss_coarse = F.binary_cross_entropy_with_logits(sal_coarse, sal_label_coarse, reduction='sum')
+                sal_final_loss = F.binary_cross_entropy_with_logits(sal_final, sal_label, reduction='sum')
+                
+                sal_loss_fuse = sal_final_loss+ 256* sal_loss_coarse
+                sal_loss = sal_loss_fuse / (self.iter_size * self.config.batch_size)
+                r_sal_loss += sal_loss.data
+                rv_loss_item+=sal_loss.item() * sal_input.size(0)
+ 
+                #mean absolute error
+                avg_mae, img_num = 0.0, 0.0
+                mea = torch.abs(sal_final - sal_label).mean()
+                if mea == mea: # for Nan
+                    avg_mae += mea
+                    img_num += 1.0
+                avg_mae /= img_num
+        
 
+                    
+        self.net.train()
+        return avg_mae.item() , rv_loss_item, r_sal_loss
     # training phase
     def train(self):
         iter_num = len(self.train_loader.dataset) // self.config.batch_size
@@ -120,24 +165,16 @@ class Solver(object):
                     device = torch.device(self.config.device_id)
                     sal_image, sal_depth, sal_label = sal_image.to(device), sal_depth.to(device), sal_label.to(device)
 
-                #sal_label_coarse = F.interpolate(sal_label, size_coarse, mode='bilinear', align_corners=True)
-                #sal_label_coarse = torch.cat((sal_label_coarse, sal_label_coarse), dim=0)
+               
                 sal_label_coarse = torch.cat((sal_label, sal_label), dim=0)
                 
                 sal_input = torch.cat((sal_image, sal_depth), dim=0)
-                sal_final,sal_lde_conv,sal_lde_tran,sal_gde_conv,sal_gde_tran, sal_coarse = self.net(sal_input)
-                sal_labels = torch.cat((sal_label, sal_label), dim=0)#need to be deleted
+                sal_final, sal_coarse = self.net(sal_input)
+                
                 sal_loss_coarse = F.binary_cross_entropy_with_logits(sal_coarse, sal_label_coarse, reduction='sum')
-                sal_loss_lde_conv = F.binary_cross_entropy_with_logits(sal_lde_conv, sal_labels, reduction='sum')
-                #print('sal_loss_lde_conv',sal_loss_lde_conv)
-                sal_loss_lde_tran = F.binary_cross_entropy_with_logits(sal_lde_tran, sal_labels, reduction='sum')
-                #print('sal_loss_lde_tran',sal_loss_lde_tran)
-                sal_loss_gde_conv= F.binary_cross_entropy_with_logits(sal_gde_conv, sal_labels, reduction='sum')
-                #print('sal_loss_gde_conv',sal_loss_gde_conv)
-                sal_loss_gde_tran = F.binary_cross_entropy_with_logits(sal_gde_tran, sal_labels, reduction='sum')
-                #print('sal_loss_gde_tran',sal_loss_gde_tran)
                 sal_final_loss = F.binary_cross_entropy_with_logits(sal_final, sal_label, reduction='sum')
-                sal_loss_fuse = sal_final_loss+sal_loss_lde_conv/4 + sal_loss_lde_tran/4 + sal_loss_gde_conv/4 + sal_loss_gde_tran/4 + sal_loss_coarse
+                
+                sal_loss_fuse = sal_final_loss+ 256* sal_loss_coarse
                 sal_loss = sal_loss_fuse / (self.iter_size * self.config.batch_size)
                 r_sal_loss += sal_loss.data
                 r_sal_loss_item+=sal_loss.item() * sal_input.size(0)
@@ -151,52 +188,247 @@ class Solver(object):
                     aveGrad = 0
 
                 if (i + 1) % (self.show_every // self.config.batch_size) == 0:
-                    print('epoch: [%2d/%2d], iter: [%5d/%5d]  ||  Sal : %10.4f ,lde_c : %.4f,lde_t: %.4f,gde_c: %.4f,gde_t: %.4f' % (
-                        epoch, self.config.epoch, i + 1, iter_num, r_sal_loss / (self.show_every / self.iter_size),sal_loss_lde_conv.data,sal_loss_lde_tran.data,sal_loss_gde_conv.data,sal_loss_gde_tran.data))
+                    print('epoch: [%2d/%2d], iter: [%5d/%5d]  ||  Sal : %10.4f ' % (
+                        epoch, self.config.epoch, i + 1, iter_num, r_sal_loss / (self.show_every / self.iter_size)))
                     # print('Learning rate: ' + str(self.lr))
                     writer.add_scalar('training loss', r_sal_loss / (self.show_every / self.iter_size),
                                       epoch * len(self.train_loader.dataset) + i)
-                    writer.add_scalar('sal_loss_coarse training loss', sal_loss_coarse,
+                    writer.add_scalar('sal_loss_coarse training loss', sal_loss_coarse.data,
                                       epoch * len(self.train_loader.dataset) + i)
-                    writer.add_scalar(' sal_loss_lde_conv training loss', sal_loss_lde_conv ,
-                                      epoch * len(self.train_loader.dataset) + i)
-                    writer.add_scalar('sal_loss_lde_tran training loss', sal_loss_lde_tran,
-                                      epoch * len(self.train_loader.dataset) + i)
-                    writer.add_scalar('sal_loss_gde_conv training loss', sal_loss_gde_conv,
-                                      epoch * len(self.train_loader.dataset) + i)
-                    writer.add_scalar(' sal_loss_gde_trantraining loss', sal_loss_gde_tran,
-                                      epoch * len(self.train_loader.dataset) + i)
+
                     r_sal_loss = 0
                     res = sal_coarse[0].clone()
                     res = res.sigmoid().data.cpu().numpy().squeeze()
                     res = (res - res.min()) / (res.max() - res.min() + 1e-8)
                     writer.add_image('sal_coarse', torch.tensor(res), i, dataformats='HW')
                     grid_image = make_grid(sal_label_coarse[0].clone().cpu().data, 1, normalize=True)
-                    res_sal_lde_conv = sal_lde_conv[0].clone()
-                    res_sal_lde_conv = res_sal_lde_conv.sigmoid().data.cpu().numpy().squeeze()
-                    res_sal_lde_conv = (res_sal_lde_conv - res_sal_lde_conv.min()) / (res_sal_lde_conv.max() - res_sal_lde_conv.min() + 1e-8)
-                    writer.add_image('sal_lde_conv', torch.tensor(res_sal_lde_conv), i, dataformats='HW')
-                    grid_image = make_grid(sal_label_coarse[0].clone().cpu().data, 1, normalize=True)
-                    res_sal_lde_tran = sal_lde_tran[0].clone()
-                    res_sal_lde_tran = res_sal_lde_tran.sigmoid().data.cpu().numpy().squeeze()
-                    res_sal_lde_tran = (res_sal_lde_tran - res_sal_lde_tran.min()) / (res_sal_lde_tran.max() - res_sal_lde_tran.min() + 1e-8)
-                    writer.add_image('sal_lde_tran', torch.tensor(res_sal_lde_tran), i, dataformats='HW')
-                    grid_image = make_grid(sal_label_coarse[0].clone().cpu().data, 1, normalize=True)
-                    res_sal_gde_conv = sal_gde_conv[0].clone()
-                    res_sal_gde_conv= res_sal_gde_conv.sigmoid().data.cpu().numpy().squeeze()
-                    res_sal_gde_conv = (res_sal_gde_conv - res_sal_gde_conv.min()) / (res_sal_gde_conv.max() - res_sal_gde_conv.min() + 1e-8)
-                    writer.add_image('sal_gde_conv', torch.tensor(res_sal_gde_conv), i, dataformats='HW')
-                    grid_image = make_grid(sal_label_coarse[0].clone().cpu().data, 1, normalize=True)
-                    res_sal_gde_tran = sal_gde_tran[0].clone()
-                    res_sal_gde_tran= res_sal_gde_tran.sigmoid().data.cpu().numpy().squeeze()
-                    res_sal_gde_tran = (res_sal_gde_tran - res_sal_gde_tran.min()) / (res_sal_gde_tran.max() - res_sal_gde_tran.min() + 1e-8)
-                    writer.add_image('sal_gde_tran', torch.tensor(res_sal_gde_tran), i, dataformats='HW')
-                    grid_image = make_grid(sal_label_coarse[0].clone().cpu().data, 1, normalize=True)
+                    
+                    fsal = sal_final[0].clone()
+                    fsal = fsal.sigmoid().data.cpu().numpy().squeeze()
+                    fsal = (fsal - fsal.min()) / (fsal.max() - fsal.min() + 1e-8)
+                    writer.add_image('sal_final', torch.tensor(fsal), i, dataformats='HW')
+                    grid_image = make_grid(sal_label[0].clone().cpu().data, 1, normalize=True)
+
 
             if (epoch + 1) % self.config.epoch_save == 0:
                 torch.save(self.net.state_dict(), '%s/epoch_%d.pth' % (self.config.save_folder, epoch + 1))
             train_loss=r_sal_loss_item/len(self.train_loader.dataset)
             loss_vals.append(train_loss)
-
+            mae,sMeasure,eMeasure,fMeasure,loss_val,loss_val_r = self.val()
+            print('Epoch:[%2d/%2d] | Train Loss : %.3f' % (epoch, self.config.epoch,train_loss))
+            print('Epoch:[%2d/%2d] | Validation Loss : %.3f | mae : %.3f|sMeasure: %.3f|eMeasure : %.3f|fMeasure: %.3f' % (epoch, self.config.epoch,loss_val_r,mae,sMeasure,eMeasure,fMeasure))
+            writer.add_scalar('validation loss', loss_val_r ,epoch * len(self.val_loader.dataset) + i)
         # save model
         torch.save(self.net.state_dict(), '%s/final.pth' % self.config.save_folder)
+        
+
+    
+import os
+import time
+from sklearn import metrics
+import numpy as np
+import torch
+from torchvision import transforms
+
+
+    
+    def Eval_fmeasure(self):
+        print('eval[FMeasure]:{} dataset .'.format(self.dataset))
+        beta2 = 0.3
+        avg_f, img_num = 0.0, 0.0
+
+        with torch.no_grad():
+            trans = transforms.Compose([transforms.ToTensor()])
+            for pred, gt in self.loader:
+                if self.cuda:
+                    pred = trans(pred).cuda()
+                    gt = trans(gt).cuda()
+                else:
+                    pred = trans(pred)
+                    gt = trans(gt)
+                prec, recall = self._eval_pr(pred, gt, 255)
+                f_score = (1 + beta2) * prec * recall / (beta2 * prec + recall)
+                f_score[f_score != f_score] = 0 # for Nan
+                avg_f += f_score
+                img_num += 1.0
+                score = avg_f / img_num
+            return score.max().item()
+    def Eval_Emeasure(self):
+        print('eval[EMeasure]:{} dataset .'.format(self.dataset))
+        avg_e, img_num = 0.0, 0.0
+        with torch.no_grad():
+            trans = transforms.Compose([transforms.ToTensor()])
+            scores = torch.zeros(255)
+            if self.cuda:
+                scores = scores.cuda()
+            for pred, gt in self.loader:
+                if self.cuda:
+                    pred = trans(pred).cuda()
+                    gt = trans(gt).cuda()
+                else:
+                    pred = trans(pred)
+                    gt = trans(gt)
+                scores += self._eval_e(pred, gt, 255)
+                img_num += 1.0
+                
+            scores /= img_num
+            return scores.max().item()
+    def Eval_Smeasure(self):
+        print('eval[SMeasure]:{} dataset .'.format(self.dataset))
+        alpha, avg_q, img_num = 0.5, 0.0, 0.0
+        with torch.no_grad():
+            trans = transforms.Compose([transforms.ToTensor()])
+            for pred, gt in self.loader:
+                if self.cuda:
+                    pred = trans(pred).cuda()
+                    gt = trans(gt).cuda()
+                else:
+                    pred = trans(pred)
+                    gt = trans(gt)
+                y = gt.mean()
+                if y == 0:
+                    x = pred.mean()
+                    Q = 1.0 - x
+                elif y == 1:
+                    x = pred.mean()
+                    Q = x
+                else:
+                    gt[gt>=0.5] = 1
+                    gt[gt<0.5] = 0
+                    #print(self._S_object(pred, gt), self._S_region(pred, gt))
+                    Q = alpha * self._S_object(pred, gt) + (1-alpha) * self._S_region(pred, gt)
+                    if Q.item() < 0:
+                        Q = torch.FloatTensor([0.0])
+                img_num += 1.0
+                avg_q += Q.item()
+            avg_q /= img_num
+            return avg_q
+    def LOG(self, output):
+        with open(self.logfile, 'a') as f:
+            f.write(output)
+
+    def _eval_e(self, y_pred, y, num):
+        if self.cuda:
+            score = torch.zeros(num).cuda()
+            thlist = torch.linspace(0, 1 - 1e-10, num).cuda()
+        else:
+            score = torch.zeros(num)
+            thlist = torch.linspace(0, 1 - 1e-10, num)
+        for i in range(num):
+            y_pred_th = (y_pred >= thlist[i]).float()
+            fm = y_pred_th - y_pred_th.mean()
+            gt = y - y.mean()
+            align_matrix = 2 * gt * fm / (gt * gt + fm * fm + 1e-20)
+            enhanced = ((align_matrix + 1) * (align_matrix + 1)) / 4
+            score[i] = torch.sum(enhanced) / (y.numel() - 1 + 1e-20)
+        return score
+
+    def _eval_pr(self, y_pred, y, num):
+        if self.cuda:
+            prec, recall = torch.zeros(num).cuda(), torch.zeros(num).cuda()
+            thlist = torch.linspace(0, 1 - 1e-10, num).cuda()
+        else:
+            prec, recall = torch.zeros(num), torch.zeros(num)
+            thlist = torch.linspace(0, 1 - 1e-10, num)
+        for i in range(num):
+            y_temp = (y_pred >= thlist[i]).float()
+            tp = (y_temp * y).sum()
+            prec[i], recall[i] = tp / (y_temp.sum() + 1e-20), tp / (y.sum() + 1e-20)
+        return prec, recall
+    
+    def _S_object(self, pred, gt):
+        fg = torch.where(gt==0, torch.zeros_like(pred), pred)
+        bg = torch.where(gt==1, torch.zeros_like(pred), 1-pred)
+        o_fg = self._object(fg, gt)
+        o_bg = self._object(bg, 1-gt)
+        u = gt.mean()
+        Q = u * o_fg + (1-u) * o_bg
+        return Q
+
+    def _object(self, pred, gt):
+        temp = pred[gt == 1]
+        x = temp.mean()
+        sigma_x = temp.std()
+        score = 2.0 * x / (x * x + 1.0 + sigma_x + 1e-20)
+        
+        return score
+
+    def _S_region(self, pred, gt):
+        X, Y = self._centroid(gt)
+        gt1, gt2, gt3, gt4, w1, w2, w3, w4 = self._divideGT(gt, X, Y)
+        p1, p2, p3, p4 = self._dividePrediction(pred, X, Y)
+        Q1 = self._ssim(p1, gt1)
+        Q2 = self._ssim(p2, gt2)
+        Q3 = self._ssim(p3, gt3)
+        Q4 = self._ssim(p4, gt4)
+        Q = w1*Q1 + w2*Q2 + w3*Q3 + w4*Q4
+        # print(Q)
+        return Q
+    
+    def _centroid(self, gt):
+        rows, cols = gt.size()[-2:]
+        gt = gt.view(rows, cols)
+        if gt.sum() == 0:
+            if self.cuda:
+                X = torch.eye(1).cuda() * round(cols / 2)
+                Y = torch.eye(1).cuda() * round(rows / 2)
+            else:
+                X = torch.eye(1) * round(cols / 2)
+                Y = torch.eye(1) * round(rows / 2)
+        else:
+            total = gt.sum()
+            if self.cuda:
+                i = torch.from_numpy(np.arange(0,cols)).cuda().float()
+                j = torch.from_numpy(np.arange(0,rows)).cuda().float()
+            else:
+                i = torch.from_numpy(np.arange(0,cols)).float()
+                j = torch.from_numpy(np.arange(0,rows)).float()
+            X = torch.round((gt.sum(dim=0)*i).sum() / total)
+            Y = torch.round((gt.sum(dim=1)*j).sum() / total)
+        return X.long(), Y.long()
+    
+    def _divideGT(self, gt, X, Y):
+        h, w = gt.size()[-2:]
+        area = h*w
+        gt = gt.view(h, w)
+        LT = gt[:Y, :X]
+        RT = gt[:Y, X:w]
+        LB = gt[Y:h, :X]
+        RB = gt[Y:h, X:w]
+        X = X.float()
+        Y = Y.float()
+        w1 = X * Y / area
+        w2 = (w - X) * Y / area
+        w3 = X * (h - Y) / area
+        w4 = 1 - w1 - w2 - w3
+        return LT, RT, LB, RB, w1, w2, w3, w4
+
+    def _dividePrediction(self, pred, X, Y):
+        h, w = pred.size()[-2:]
+        pred = pred.view(h, w)
+        LT = pred[:Y, :X]
+        RT = pred[:Y, X:w]
+        LB = pred[Y:h, :X]
+        RB = pred[Y:h, X:w]
+        return LT, RT, LB, RB
+
+    def _ssim(self, pred, gt):
+        gt = gt.float()
+        h, w = pred.size()[-2:]
+        N = h*w
+        x = pred.mean()
+        y = gt.mean()
+        sigma_x2 = ((pred - x)*(pred - x)).sum() / (N - 1 + 1e-20)
+        sigma_y2 = ((gt - y)*(gt - y)).sum() / (N - 1 + 1e-20)
+        sigma_xy = ((pred - x)*(gt - y)).sum() / (N - 1 + 1e-20)
+        
+        aplha = 4 * x * y *sigma_xy
+        beta = (x*x + y*y) * (sigma_x2 + sigma_y2)
+
+        if aplha != 0:
+            Q = aplha / (beta + 1e-20)
+        elif aplha == 0 and beta == 0:
+            Q = 1.0
+        else:
+            Q = 0
+        return Q
